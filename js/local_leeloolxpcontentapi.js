d034dc69a3adb92d9094fdb4daa330150609a45a
anayleeloolxp <anayjain@leeloolxp.com>
2024-04-23 13:06:45 +0530

    Added code for drawer close send message to spock

--- js/local_leeloolxpcontentapi.js
+++ js/local_leeloolxpcontentapi.js
@@ -1,72 +1,82 @@
 require(["jquery"], function ($) {
-  $("#local_leeloolxpcontentapi_wrapper_close").click(function () {
-    $(".local_leeloolxpcontentapi_wrapper").removeClass("open");
-  });
+  	$("#local_leeloolxpcontentapi_wrapper_close").click(function () {
+		$(".local_leeloolxpcontentapi_wrapper").removeClass("open");
 
-  $(document).ready(function () {
+		var message = 'drawer_closed';
+		var iframe = $(".leeloolxpcontentapi_frame")[0];
 
-    if( !$('.leeloolxpcontentapi_frame').length ){
-      let mootoolsleeloourl = $("#leeloolxpcontentapi-js-vars").data(
-        "mootoolsleeloourl"
-      );
-      let mootoolsleeloourldecoded = atob(
-        $("#leeloolxpcontentapi-js-vars").data("mootoolsleeloourl")
-      );
-      let mootoolstoken = $("#leeloolxpcontentapi-js-vars").data("mootoolstoken");
-      let mootoolsresponse = $("#leeloolxpcontentapi-js-vars").data("mootoolsloginresponse");
-
-      let cmid = $("#leeloolxpcontentapi-js-vars").data("cmid");
-      let sectionid = $("#leeloolxpcontentapi-js-vars").data("sectionid");
-      let courseid = $("#leeloolxpcontentapi-js-vars").data("courseid");
-
-      var mootoolsresponseDe = JSON.parse(atob(mootoolsresponse));
-      mootoolsresponseDe.cmid = cmid;
-      mootoolsresponseDe.sectionid = sectionid;
-      mootoolsresponseDe.courseid = courseid;
-      mootoolsresponseDe.baseurl = mootoolsleeloourldecoded + '/';
-
-      var mootoolsresponseUp = btoa(JSON.stringify(mootoolsresponseDe));
-
-      leeloolxpssourl =
-        "https://spock.leeloolxp.com?mootoolsleeloourl=" +
-        mootoolsleeloourl +
-        "&mootoolstoken=" +
-        mootoolstoken +
-        "&mootoolsresponse=" +
-        mootoolsresponseUp;
-
-      document.getElementById("local_leeloolxpcontentapi_frame").innerHTML =
-        '<iframe allow="camera; microphone" src="' +
-        leeloolxpssourl +
-        '" class="leeloolxpcontentapi_frame"></iframe>';
-    }
-
-    $("#local_leeloolxpcontentapi_button").click(function () {
-
-		var $wrapper = $(".local_leeloolxpcontentapi_wrapper");
-
-		// Toggle the "open" class on the wrapper
-		$wrapper.toggleClass("open");
-
-		// Check if the wrapper has the "open" class
-		if ($wrapper.hasClass("open")) {
-
-			var message = 'drawer_open';
-			var iframe = $(".leeloolxpcontentapi_frame")[0];
-
-			// Check if the iframe is loaded
-			if (iframe.contentWindow) {
-				console.log('loadedalready');
+		// Check if the iframe is loaded
+		if (iframe.contentWindow) {
+			iframe.contentWindow.postMessage(message, "*");
+		} else {
+			// If iframe is not loaded, wait for it to load
+			$(iframe).on('load', function() {
 				iframe.contentWindow.postMessage(message, "*");
-			} else {
-				console.log('notloaded');
-				// If iframe is not loaded, wait for it to load
-				$(iframe).on('load', function() {
-					console.log('loaded');
-					iframe.contentWindow.postMessage(message, "*");
-				});
-			}
+			});
 		}
-    });
-  });
+  	});
+
+  	$(document).ready(function () {
+
+		if( !$('.leeloolxpcontentapi_frame').length ){
+			let mootoolsleeloourl = $("#leeloolxpcontentapi-js-vars").data(
+				"mootoolsleeloourl"
+			);
+			let mootoolsleeloourldecoded = atob(
+				$("#leeloolxpcontentapi-js-vars").data("mootoolsleeloourl")
+			);
+			let mootoolstoken = $("#leeloolxpcontentapi-js-vars").data("mootoolstoken");
+			let mootoolsresponse = $("#leeloolxpcontentapi-js-vars").data("mootoolsloginresponse");
+
+			let cmid = $("#leeloolxpcontentapi-js-vars").data("cmid");
+			let sectionid = $("#leeloolxpcontentapi-js-vars").data("sectionid");
+			let courseid = $("#leeloolxpcontentapi-js-vars").data("courseid");
+
+			var mootoolsresponseDe = JSON.parse(atob(mootoolsresponse));
+			mootoolsresponseDe.cmid = cmid;
+			mootoolsresponseDe.sectionid = sectionid;
+			mootoolsresponseDe.courseid = courseid;
+			mootoolsresponseDe.baseurl = mootoolsleeloourldecoded + '/';
+
+			var mootoolsresponseUp = btoa(JSON.stringify(mootoolsresponseDe));
+
+			leeloolxpssourl =
+				"https://spock.leeloolxp.com?mootoolsleeloourl=" +
+				mootoolsleeloourl +
+				"&mootoolstoken=" +
+				mootoolstoken +
+				"&mootoolsresponse=" +
+				mootoolsresponseUp;
+
+			document.getElementById("local_leeloolxpcontentapi_frame").innerHTML =
+				'<iframe allow="camera; microphone" src="' +
+				leeloolxpssourl +
+				'" class="leeloolxpcontentapi_frame"></iframe>';
+		}
+
+    	$("#local_leeloolxpcontentapi_button").click(function () {
+
+			var $wrapper = $(".local_leeloolxpcontentapi_wrapper");
+
+			// Toggle the "open" class on the wrapper
+			$wrapper.toggleClass("open");
+
+			// Check if the wrapper has the "open" class
+			if ($wrapper.hasClass("open")) {
+
+				var message = 'drawer_open';
+				var iframe = $(".leeloolxpcontentapi_frame")[0];
+
+				// Check if the iframe is loaded
+				if (iframe.contentWindow) {
+					iframe.contentWindow.postMessage(message, "*");
+				} else {
+					// If iframe is not loaded, wait for it to load
+					$(iframe).on('load', function() {
+						iframe.contentWindow.postMessage(message, "*");
+					});
+				}
+			}
+    	});
+  	});
 });
